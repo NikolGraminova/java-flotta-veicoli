9e5145146e9c7fe365458fe3643973377ab008b5
NikolGraminova <nbackup619@gmail.com>
2023-07-31 14:09:25 +0200

    Created project workspace

---
+++ src/org/java/lessons/vehicles/FleetManager.java
@@ -0,0 +1,4 @@
+package org.java.lessons.vehicles;
+
+public class FleetManager {
+}
---
+++ src/org/java/lessons/vehicles/Main.java
@@ -0,0 +1,7 @@
+package org.java.lessons.vehicles;
+
+public class Main {
+    public static void main(String[] args) {
+
+    }
+}
